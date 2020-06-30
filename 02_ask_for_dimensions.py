f31941b24d5218265ebde039728e63cea9bfc7ab
hibbertm71030 <60630392+hibbertm71030@users.noreply.github.com>
2020-07-01 10:58:23 +1200

    bruh

--- 02_ask_for_dimensions.py
+++ 02_ask_for_dimensions.py
@@ -13,14 +13,36 @@ def shape_checker(question):
         response = input(question)
 
         if response.lower() in list_of_shapes:
-            print("you chose {}".format(response.lower()))
-            break
+            return response.lower()
 
 
         elif response.lower() not in list_of_shapes:
             print(error)
             continue
 
+# not blank function
+def not_blank(question):
+    error = "please use a number"
+
+
+    valid = False
+    while not valid:
+        response = input(question)
+
+        if response is int:
+            print("nice")
+            return response
+
+        elif response is not int:
+            print(error)
+            continue
+
+
+
+
+
 # ask user for shape
 shape = shape_checker("What shape do you want to calculate for? ")
 
+if shape == "square":
+    not_blank("what is the base? (please use same units for base and height) ")
